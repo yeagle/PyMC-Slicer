0847d988ae7e5ffb6b4bf503027f91bbf71a0898
Dominik Wabersich <dominik.wabersich@gmail.com>
2013-02-07 14:22:22 -0800

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# __init__.py
+#
+# (c) 2012 Dominik Wabersich <dominik.wabersich [aet] gmail.com>
+# GPL 3.0+ or (cc) by-sa (http://creativecommons.org/licenses/by-sa/3.0/)
+#
+# created 2013-02-07
+# last mod 2013-02-07 14:21 DW
+#
+
+from .SliceSampler import Slicer
+
+__all__ = ["Slicer"]
